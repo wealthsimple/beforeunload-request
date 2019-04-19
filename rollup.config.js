93be30b7519d2749742d468b3bea87c91089e7d9
Alex Turpin <alex.turpin@gmail.com>
2019-04-19 16:09:29 -0400

    Config & build scripts

---
+++ rollup.config.js
@@ -0,0 +1,39 @@
+import pkg from './package.json';
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import { uglify } from 'rollup-plugin-uglify';
+import license from 'rollup-plugin-license';
+
+export default [
+	{
+		input: 'src/index.js',
+		output: { file: pkg.module, format: 'es' },
+		plugins: [
+			resolve(),
+			license({
+				banner: {
+					file: './LICENSE'
+				}
+			})
+		]
+	},
+	{
+		input: 'src/index.js',
+		output: {
+			name: 'beforeunload-request',
+			file: pkg.browser,
+			format: 'umd',
+			sourcemap: true
+		},
+		plugins: [
+			resolve(),
+			babel({ exclude: 'node_modules/**' }),
+			uglify(),
+			license({
+				banner: {
+					file: './LICENSE'
+				}
+			})
+		]
+	}
+];
